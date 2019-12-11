7086cec0ec7a86508da200a26a3e94184431b72f
Владимир Ю. Ивахненко <ivakhnenko@yamoney.ru>
2019-12-12 01:35:18 +0300

    Updates

---
+++ precache-manifest.3ae7a248d52d8de06d010b279f9bb0a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b7b81d881dc2c2546aa184730c0d9c",
+    "url": "/flashlight/index.html"
+  },
+  {
+    "revision": "04304932f2d3193c9860",
+    "url": "/flashlight/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "7e479c6da5645249dfad",
+    "url": "/flashlight/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "04304932f2d3193c9860",
+    "url": "/flashlight/static/js/2.c6efdfbe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/flashlight/static/js/2.c6efdfbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e479c6da5645249dfad",
+    "url": "/flashlight/static/js/main.f4454802.chunk.js"
+  },
+  {
+    "revision": "9a97fc99c36fdc6d9bd8",
+    "url": "/flashlight/static/js/runtime-main.a059d3de.js"
+  }
+]);
